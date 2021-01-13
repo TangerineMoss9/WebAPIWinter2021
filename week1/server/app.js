618321eb26f69495379b2dafcd709fce395a3a9c
TangerineMoss9 <TangerineMoss9@gmail.com>
2021-01-13 13:05:16 -0500

    third commit

--- week1/server/app.js
+++ week1/server/app.js
@@ -1,6 +1,11 @@
 // app is the entry point of application
-function sendMsg(){
-    console.log("Hello World")
-}
+var http = require('http')
 
-sendMsg()
+http.createServer(function(request, response){
+    //http header
+    response.writeHead(200,{'Content-type':'text/plain'})
+    //Send a responce to the body of the html
+    response.end("Wouldn't you like to be a pepper too???")
+}).listen(3000)
+
+console.log("Server is running on port 3000")
